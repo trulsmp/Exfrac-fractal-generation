204e8921868a1d6dbe2e69e0302a9b3025f24bc1
Truls Mørk Pettersen <trulsmp@stud.ntnu.no>
2015-08-26 15:59:42 +1000

    main arguments added

--- main.c
+++ main.c
@@ -2,52 +2,64 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-int main()
+
+int main(int argc, char *argv[])
 {
-    FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
-    int resolution = 800;
+    int resX;
+    int resY;
+    int max_iteration;
+    double xLow;
+    double yLow;
+    double xHigh = 2;
+    double yHigh = 2;
     
-    double passes = 0.005;
-    int pixel = (int) 4/passes;
-    printf("%d ",pixel);
-    (void) fprintf(fp, "P6\n%d %d\n255\n", pixel, pixel);
+    if (argc < 2) {
+        printf("%s","No input arguments given, setting to default values of 800x800 and 1000 iterations \n");
+        printf("%s","To set parameters yourself, where ./a.out 1 2 3 4 5 is the different values. \n 1 = X resolution, \n 2 = Y resolution, \n 3 = max iterations, \n 4/5 - start points for algorithm between -2 and 2, 4 = X, 5 = Y");
+        resX = 800;
+        resY = 800;
+        max_iteration = 1000;
+        double xLow = -2;
+        double yLow = -2;
+    }
+    else {
+        resX = atoi(argv[1]);
+        resY = atoi(argv[2]);
+        max_iteration = atoi(argv[3]);
+        if {argc > 5;
+        double xLow = arg[4];
+        double yLow = arg[5];
+        
+    }
+    
+    FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
+    (void) fprintf(fp, "P6\n%d %d\n255\n", resX, resY);
     
     double xLow = -2;
     double yLow = -2;
     double xHigh = 2;
     double yHigh = 2;
     
-    double array[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    static unsigned char blackColor[3] = {0,0,0};
     
-    
-    
-    static unsigned char greenColor[3] = {0,255,0};
-    static unsigned char blueColor[3] = {0,0,255};
-    static unsigned char redColor[3] = {255,0,0};
-    static unsigned char yellowColor[3] = {173,255,47};
-    static unsigned char orangeColor[3] = {255,140,0};
-
-    
-    int max_iteration;
     int iteration;
 
     double x;
     double y;
     double TEMPX;
+    double y0;
+    double x0;
+    int N = 256;
+    int N3 = N*N*N;
     
-    int numberOfPasses = 0;
-    for (double y0 = yLow; y0 <= yHigh; y0+=passes) {
-        for (double x0 = xLow; x0 <= xHigh; x0+=passes) {
+    for (double i = 0; i < resY; i+=1) {
+        for (double j = 0; j < resX; j+=1) {
+            y0 = ((i/resY)*4)+yLow;
+            x0 = ((j/resX)*4)+xLow;
             y = 0;
             x = 0;
-            numberOfPasses = 0;
-            printf("%s","x value:");
-            printf("%f ",x0);
-            printf("%s","y value:");
-            printf("%f ",y0);
-            printf("%s ","\n");
-            max_iteration = 300;
             iteration = 0;
+            
             while ( x*x + y*y < 4  &&  iteration < max_iteration ) {
                 TEMPX = x*x - y*y + x0;
                 y = 2*x*y + y0;
@@ -55,31 +67,21 @@ int main()
                 iteration++;
             }
             
-            printf("%s ","Number of passes:");
-            printf("%d ",numberOfPasses);
-//            
-//            if (iteration == 1) {
-//                /* color the pixel red */
-//                (void) fwrite(redColor, 1, 3, fp);
-//            }
-//            else if (iteration == 2) {
-//                /*color the pixel blue */
-//                (void) fwrite(blueColor, 1, 3, fp);
-//            }
-//            else if (iteration == 3) {
-//                /* color the pixel green */
-//                (void) fwrite(greenColor, 1, 3, fp);
-//            }
             
-            if (iteration == 1) {
-                (void) fwrite(greenColor, 1, 3, fp);
+            if(iteration == max_iteration) {
+                (void) fwrite(blackColor, 1, 3, fp);
             }
-            else if (iteration < 300) {
-                (void) fwrite(yellowColor, 1, 3, fp);
+            else {
+                double t = (double)iteration/(double)max_iteration;
+                iteration = (int)(t* (double) N3);
                 
-            }
-            else if(iteration == 300) {
-                (void) fwrite(orangeColor, 1, 3, fp);
+                int blue = iteration/(N*N);
+                int nn = iteration - blue * N * N;
+                int red = nn/N;
+                int green = nn - red * N;
+                
+                char Color[3] = {red,green,blue};
+                (void) fwrite(Color, 1, 3, fp);
             }
             
         }
